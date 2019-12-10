d75c3d1665733e9f4694faba22b34082fc306530
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-12-09 21:45:09 -0500

    practice with structs

--- Week3/psets/resize/struct.c
+++ Week3/psets/resize/struct.c
@@ -11,5 +11,12 @@ int main(void)
     MYSTRUCT item;
     item.val1 = 18;
     item.val2 = 38;
+    printf("%i\n", item.val2);
+
+    MYSTRUCT structArray[5];
+    structArray[0].val1 = 48;
+    structArray[0].val2 = 88;
+    structArray[3].val2 = 33;
+    printf("%i\n", structArray[0].val1);
     return 0;
 }
